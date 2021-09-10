e6f14c7d7d2f5be4da31bdde6aa277b6dd339390
rahimBasha <rahimdev786@gmail.com>
2021-09-10 12:08:24 +0530

    Initial Commit

---
+++ Closure_Examples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closure_Examples
+//
+//  Created by shaik Arshad on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
